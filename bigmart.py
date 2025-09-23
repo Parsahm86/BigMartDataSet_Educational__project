a6222cc256883af06cc66e96ffea9769d0e670b6
Seko <parsahm2016@gmail.com>
2025-09-23 18:40:42 +0330

    test-1

--- bigmart.py
+++ bigmart.py
@@ -13,6 +13,8 @@ from sklearn.linear_model import LinearRegression,SGDRegressor, Ridge, ElasticNe
 from xgboost import XGBRFRegressor
 from sklearn.ensemble import RandomForestRegressor
 
+# hello seko
+
 # ---------------------------
 
 # --------> download & load the DataSet
